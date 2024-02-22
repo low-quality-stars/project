ee2144bf5421ecc3ac5932233a2fca2cb3f8b336
low-quality-stars <felicity.taylor@gctcok.edu>
2024-02-22 10:42:24 -0600

    Update Program.cs

--- Variables_Taylor_Felicity/Variables_Taylor_Felicity/Program.cs
+++ Variables_Taylor_Felicity/Variables_Taylor_Felicity/Program.cs
@@ -1,2 +1,35 @@
-﻿int favoriteNumber = 
+﻿//system namespace to use console class
+using System;
 
+//collection of classes for this project
+namespace Variables_Taylor_Felicity
+{
+    //main class for program
+    internal class Program
+    {
+
+        //Main method excecuted when app atarts
+        static void Main(string[] args)
+        {
+            //declars int named favoriteNumber
+            int favoriteNumber;
+            //boolean initialized to false for jumping and running
+            bool isJumping = false, isRunning = false;
+            //declared random float variable
+            float myFloat;
+            //assignging favoriteNumber to 32
+            favoriteNumber = 32;
+            //assigning myFloat to 52.53
+            myFloat = 52.53f;
+            //initializing constant finalGrade to 82
+            const double finalGrade = 82.0;
+
+            //prints variables to console
+            Console.WriteLine(favoriteNumber);
+            Console.WriteLine(isJumping);
+            Console.WriteLine(isRunning);
+            Console.WriteLine(myFloat);
+            Console.WriteLine(finalGrade);
+        }  
+    }
+}

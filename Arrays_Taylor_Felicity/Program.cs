71497e0d2ee188494c79ed773dbf8ec7869609ad
low-quality-stars <felicity.taylor@gctcok.edu>
2024-05-10 09:22:03 -0500

    array comments

--- Arrays_Taylor_Felicity/Program.cs
+++ Arrays_Taylor_Felicity/Program.cs
@@ -10,7 +10,7 @@ namespace Arrays_Taylor_Felicity
         //Main method excecuted when app atarts
         static void Main(string[] args)
         {
-            //asks user for whole number, 
+            //asks user for whole number for array size, reads number and makes it the array size
             Console.WriteLine("Enter a whole number for the array size.");
             int arraySize = Convert.ToInt32(Console.ReadLine());
             int[] numbers = new int[arraySize];
@@ -18,15 +18,19 @@ namespace Arrays_Taylor_Felicity
 
             for(int i = 0; i < numbers.Length; i++)
             {
+                //asks user for a whole number again and again untl get to the size of the array-
                 Console.WriteLine("Enter a whole number to add to the array.");
                 numbers[i] = Convert.ToInt32(Console.ReadLine());
                 sum += numbers[i];
             }
 
+            //tells user the sum of the numbers
             Console.WriteLine($"The sum of the array is {sum}");
 
+            //sorts the numbers in ascending order
             Array.Sort( numbers );
 
+            //tells the user what order of the array is
             Console.WriteLine("Array:");
             foreach( int number in numbers)
             {
